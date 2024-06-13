626c7dd34c1da0e2ae557c2a3d4903716ad45bfa
yamini-sangam <85620364+yamini-sangam@users.noreply.github.com>
2024-06-13 15:15:15 +0530

    first commit

---
+++ 100-programs/1-string-functions.js
@@ -0,0 +1,14 @@
+var firstname = "Yamini";
+var lastname = "Sangam";
+var fullName = firstname + " " + lastname
+console.log(fullName)
+
+var ucName = fullName.toUpperCase();
+console.log(ucName)
+
+var lcName = fullName.toLowerCase();
+console.log(lcName)
+
+var data= "yamini sangam"
+var dataArray = data.split(" ")
+console.log(dataArray)
